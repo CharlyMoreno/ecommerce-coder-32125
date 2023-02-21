0323c6736d4961bb55685309c44887004be43f01
CharlyMoreno <carlitosmoreno12@hotmail.com>
2023-02-21 18:03:18 -0300

    Añado Login y Register

---
+++ server/services/auth.services.js
@@ -0,0 +1,44 @@
+const UsuariosDaoFactory = require("../daos/factory/users.daoFactory");
+const usuariosDao = UsuariosDaoFactory.getDao();
+
+const UnauthorizedException = require('../utils/exceptions/auth.exceptions')
+const {generateToken} = require('../utils/generateToken')
+const bCrypt = require('bcrypt');
+
+function createHash(password) {
+    return bCrypt.hashSync(
+        password,
+        bCrypt.genSaltSync(10),
+        null);
+}
+
+const register = async (user) => {
+    const isUsernameExists = await usuariosDao.getByUsername(user.username)
+    if(!isUsernameExists) {
+        const usuario = {
+            nombre:user.nombre,
+            username:user.username,
+            password: createHash(user.password),
+            email:user.email,
+            numeroTel: user.numeroTel,
+            direccion:user.direccion,
+            pais:user.pais,
+            fotoPerfil: user.fotoPerfil
+        }
+        await usuariosDao.save(usuario)
+    }
+    else throw new UnauthorizedException("Ya existe un usuario con ese username")
+}
+
+const login = async (username,password) =>{
+    const user = await usuariosDao.getByUsername(username)
+    const validPassword = user === null ? false : await bCrypt.compare(password,user.password)
+    if(validPassword){
+        const jwtUser = generateToken(user)
+        return jwtUser
+    }
+    else throw new UnauthorizedException("Username o Password incorrecta")
+}
+
+module.exports = {register,login}
+
