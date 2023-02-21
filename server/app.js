37e872da1179c3e03f1b2923e441ce5e58fb5d4a
CharlyMoreno <carlitosmoreno12@hotmail.com>
2023-02-21 18:03:51 -0300

    Agrego link de rutas de auth

--- server/app.js
+++ server/app.js
@@ -1,14 +1,17 @@
 const express = require('express')
+const { authMiddleware } = require('./middleware/auth.middleware')
 const app = express()
 
 //___________________________________________  ROUTES  _________________________________________________ //
 const apiProductosRoutes = require('./routes/api.productos.routes')
+const authRoutes = require('./routes/auth.routes')
 
 app.use(express.static('public'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.use('/api/productos',apiProductosRoutes)
+app.use('/api/auth',authRoutes)
 
 app.get('/',(req, res) => {
     res.status(200).json({"mensaje":"Hola Mundo"})
