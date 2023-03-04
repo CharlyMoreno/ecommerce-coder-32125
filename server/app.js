24910e3ac96dacf7c7254b66c7361412359b35cc
CharlyMoreno <carlitosmoreno12@hotmail.com>
2023-03-04 12:35:57 -0300

    Añado configuracion de handlebars

--- server/app.js
+++ server/app.js
@@ -1,24 +1,28 @@
 const express = require('express')
 const { authMiddleware } = require('./middleware/auth.middleware')
 const app = express()
-
+const {engine} = require('express-handlebars')
 //___________________________________________  ROUTES  _________________________________________________ //
 const apiProductosRoutes = require('./routes/api.productos.routes')
 const authRoutes = require('./routes/auth.routes')
 const apiCarritoRoutes = require('./routes/api.carrito.routes')
 const apiMensajes = require('./routes/api.mensajes.routes')
+const infoRoutes = require('./routes/info.routes')
 
 app.use(express.static('public'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+// ________________________ HANDLEBARS __________________________
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', __dirname + '/views');
+
 app.use('/api/productos',apiProductosRoutes)
 app.use('/api/auth',authRoutes)
 app.use('/api/carrito', apiCarritoRoutes)
 app.use('/api/chat', apiMensajes)
-
-app.get('/',(req, res) => {
-    res.status(200).json({"mensaje":"Hola Mundo"})
-})
+app.use('/info',infoRoutes)
 
 module.exports = {app}
+ 
